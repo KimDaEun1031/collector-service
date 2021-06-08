b2cba15fc57d6d03c6f32f3fc70e3a4d15d2ee9b
KimDaEun1031 <kimde1031@gmail.com>
2021-06-08 23:44:34 +0900

    test commit

---
+++ src/main/java/org/daeun/sboot/SbootApplication.java
@@ -0,0 +1,13 @@
+package org.daeun.sboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/daeun/sboot/SbootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.daeun.sboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
