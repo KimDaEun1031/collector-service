57a2c34f0fd71a4948b86953447c2cff2eed8294
KimDaEun1031 <kimde1031@gmail.com>
2021-06-30 21:08:47 +0900

    url , style change

--- src/main/java/org/daeun/sboot/controller/CovidVaccineStatController.java
+++ src/main/java/org/daeun/sboot/controller/CovidVaccineStatController.java
@@ -1,11 +1,13 @@
 package org.daeun.sboot.controller;
 
 import java.net.URI;
+import java.net.URLEncoder;
 import java.util.HashMap;
 import java.util.Map;
 
 import javax.print.DocFlavor.STRING;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -21,23 +23,22 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 
 @RestController
-public class RestTestController {
+@Slf4j
+public class CovidVaccineStatController {
 
-	@GetMapping("/covid_vaccine_stat")
-	public String callAPI(@RequestParam String month, String day) {
+	@GetMapping("/covidVaccineStat")
+	public void callAPI(@RequestParam String month, String day, String sido) {
 		Map<String, Object> result = new HashMap<String, Object>();
-		
-		String jsonInString = "";
 
 		try {
 			RestTemplate restTemplate = new RestTemplate();
-			
+
 			String url = "https://api.odcloud.kr/api/15077756/v1/vaccine-stat?"
 					+ "page=1"
 					+ "&perPage=18"
 					+ "&cond%5BbaseDate%3A%3AEQ%5D=2021-"+ month +"-"+ day +"%2000%3A00%3A00"
-					+ "&serviceKey=HGz5UDF80tY61L5yPZe3Ji96a0VZwzAzSwwlbvkRjxMAscm3dZybsbX2v4HlACe%2BBgRhZT2LpzY6VV9D6bjJyg%3D%3D";
-			
+					+ "&cond%5Bsido%3A%3AEQ%5D=" + URLEncoder.encode(sido, "UTF-8") + "&serviceKey=HGz5UDF80tY61L5yPZe3Ji96a0VZwzAzSwwlbvkRjxMAscm3dZybsbX2v4HlACe%2BBgRhZT2LpzY6VV9D6bjJyg%3D%3D";
+
 			HttpHeaders header = new HttpHeaders();
 			HttpEntity<?> entity = new HttpEntity<>(header);
 
@@ -48,20 +49,19 @@ public class RestTestController {
             result.put("body", resultMap.getBody());
             
             ObjectMapper mapper = new ObjectMapper();
-            jsonInString = mapper.writeValueAsString(resultMap.getBody());
+            String jsonInString = mapper.writeValueAsString(resultMap.getBody());
            
 		} catch (HttpClientErrorException | HttpServerErrorException e) {
 			result.put("statusCode", e.getRawStatusCode());
-            result.put("body"  , e.getStatusText());
-            System.out.println(result);
+			result.put("body", e.getStatusText());
+			log.info(e.toString());
 	 
 		} catch (Exception e) {
-            result.put("statusCode", "999");
-            result.put("body"  , "excpetion오류");
-            System.out.println(e.toString());
+			result.put("statusCode", "999");
+			result.put("body", "excpetion 오류");
+			log.info(e.toString());
         }
- 
-        return jsonInString;
+
 	}
 	
 
