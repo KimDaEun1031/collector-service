f23960974be595524ac6bd96b922f5dbec3983d6
KimDaEun1031 <kimde1031@gmail.com>
2021-06-30 23:40:11 +0900

    modify

--- src/main/java/org/daeun/sboot/controller/CovidVaccineStatController.java
+++ src/main/java/org/daeun/sboot/controller/CovidVaccineStatController.java
@@ -5,8 +5,6 @@ import java.net.URLEncoder;
 import java.util.HashMap;
 import java.util.Map;
 
-import javax.print.DocFlavor.STRING;
-
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -27,42 +25,48 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 public class CovidVaccineStatController {
 
 	@GetMapping("/covidVaccineStat")
-	public void callAPI(@RequestParam String month, String day, String sido) {
+	public String callAPI(@RequestParam String month, String day, String sido) {
 		Map<String, Object> result = new HashMap<String, Object>();
 
+		String test = "";
 		try {
 			RestTemplate restTemplate = new RestTemplate();
+
+			//포맷으로 다시 정리
+			//복사해서 만들기
 
 			String url = "https://api.odcloud.kr/api/15077756/v1/vaccine-stat?"
 					+ "page=1"
 					+ "&perPage=18"
 					+ "&cond%5BbaseDate%3A%3AEQ%5D=2021-"+ month +"-"+ day +"%2000%3A00%3A00"
 					+ "&cond%5Bsido%3A%3AEQ%5D=" + URLEncoder.encode(sido, "UTF-8") + "&serviceKey=HGz5UDF80tY61L5yPZe3Ji96a0VZwzAzSwwlbvkRjxMAscm3dZybsbX2v4HlACe%2BBgRhZT2LpzY6VV9D6bjJyg%3D%3D";
-
+			//로그 쓰기
 			HttpHeaders header = new HttpHeaders();
 			HttpEntity<?> entity = new HttpEntity<>(header);
-
+			//로그로 데이터를 가져왔습니다라고 쓸듯
 			ResponseEntity<Map> resultMap = restTemplate.exchange(URI.create(url), HttpMethod.GET, entity, Map.class);			
 			
 			result.put("statusCode", resultMap.getStatusCodeValue());
             result.put("header", resultMap.getHeaders());
             result.put("body", resultMap.getBody());
-            
+
             ObjectMapper mapper = new ObjectMapper();
-            String jsonInString = mapper.writeValueAsString(resultMap.getBody());
+            test = mapper.writeValueAsString(resultMap.getBody());
+            //스트링 로그로
            
 		} catch (HttpClientErrorException | HttpServerErrorException e) {
 			result.put("statusCode", e.getRawStatusCode());
 			result.put("body", e.getStatusText());
-			log.info(e.toString());
-	 
+
 		} catch (Exception e) {
 			result.put("statusCode", "999");
 			result.put("body", "excpetion 오류");
 			log.info(e.toString());
         }
 
+		return test;
 	}
-	
+
+
 
 }
